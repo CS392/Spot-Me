664fcabf616fb075cf606437afa078a7e97c198d
Hou chi Chan <houchichan@Hous-MacBook-Air.local>
2023-11-06 12:24:23 -0500

    added front-end files

---
+++ Spot-Me/ClientApp/src/pages/Login.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import "../assets/css/loginPage.css";
+export default class Login extends Component {
+    render() {
+        return (
+            <main> 
+                <section>
+                    <h1> Login </h1>
+                    <div>
+                        <h4> User Name </h4>
+                        <input type={"text"} placeholder={"User Name"}/>
+                        <h4> Password </h4>
+                        <input type={"text"} placeholder={"Password"}/>
+                    </div>
+                    <button> Login </button>
+                    <hr/>
+                    <p> 
+                        No account yet? <a href={"#"}> Sign Up </a>
+                    </p>
+                </section>
+            </main>
+        );
+    }
+}
