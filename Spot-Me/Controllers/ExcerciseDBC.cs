12ac45c1d0c691b0ad69746229478f74d6436ad2
Yu Zeng <zengy2003@gmail.com>
2023-11-08 17:35:01 -0500

    MVC In progress

---
+++ Spot-Me/Controllers/ExcerciseDBC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spot_Me.Models;
+using Spot_Me.Services;
+
+namespace Spot_Me.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ExerciseModel _exerciseModel;
+
+        public HomeController(ExerciseModel exerciseModel)
+        {
+            _exerciseModel = exerciseModel;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string exerciseType = "back";
+
+            try
+            {
+                await _exerciseModel.UpdateResult(exerciseType);
+            }
+            catch (Exception error)
+            {
+                Console.Error.WriteLine("Error: " + error.Message);
+            }
+
+            return View();
+        }
+    }
+
+}
