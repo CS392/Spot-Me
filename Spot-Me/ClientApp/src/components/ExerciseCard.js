1a229bde511c91cea2c25e18221bc1e745e482bc
Yu Zeng <zengy2003@gmail.com>
2023-11-14 20:01:44 -0500

    Picture display

--- Spot-Me/ClientApp/src/components/ExerciseCard.js
+++ Spot-Me/ClientApp/src/components/ExerciseCard.js
@@ -1,48 +1,41 @@
 import React, { Component } from "react";
 
 export class ExerciseCard extends Component {
-    static description;
-    static imageUrl;
-
     constructor(props) {
         super(props);
-        ExerciseCard.description = this.props.desc;
-        ExerciseCard.imageUrl = this.props?.imageUrl;
+        this.state = {
+            exercises: []
+        };
     }
 
     componentDidMount() {
-        // Call your API here and update state with the result
         this.fetchExerciseData();
     }
 
     async fetchExerciseData() {
         try {
-            
-            const response = await fetch("api/excerciseDB");
-            console.log("check", response);
+            const response = await fetch('https://localhost:7229/api/excerciseDB/data');
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-
             const data = await response.json();
-            console.log("show",data.json());
-            // Assuming your API returns an object with imageUrl and description
-            ExerciseCard.imageUrl = data.imageUrl;
-            ExerciseCard.description = data.description;
-
-            // Force a re-render by updating the component state
-            this.forceUpdate();
+            this.setState({ exercises: data });
         } catch (error) {
             console.error("Error fetching exercise data:", error.message);
         }
     }
 
     render() {
+        const { exercises } = this.state;
+
         return (
-            <div>
-                {/* Change this to image HTML tag */}
-                {ExerciseCard.imageUrl !== undefined && <p> {ExerciseCard.imageUrl} </p>}
-                <p> {ExerciseCard.description} </p>
+            <div className="exercise-list">
+                {exercises.map((exercise, index) => (
+                    <div key={index}>
+                        <img src={exercise.gifUrl} alt="Exercise" />
+                        <p>{`${exercise.bodyPart} - ${exercise.equipment} - ${exercise.name}`}</p>
+                    </div>
+                ))}
             </div>
         );
     }
